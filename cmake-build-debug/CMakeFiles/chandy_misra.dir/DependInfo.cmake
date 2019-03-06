9d4ad6486fca3c232cb77357a1ae7657e53b1087
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-03-06 23:11:43 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/chandy_misra.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jakub/dev/repos/chandy-misra/Coordinator.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/Coordinator.cpp.o"
+  "/home/jakub/dev/repos/chandy-misra/Fork.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/Fork.cpp.o"
+  "/home/jakub/dev/repos/chandy-misra/Philosopher.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/Philosopher.cpp.o"
+  "/home/jakub/dev/repos/chandy-misra/Table.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/Table.cpp.o"
+  "/home/jakub/dev/repos/chandy-misra/ThreadSafePrinter.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/ThreadSafePrinter.cpp.o"
+  "/home/jakub/dev/repos/chandy-misra/Visualisation.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/Visualisation.cpp.o"
+  "/home/jakub/dev/repos/chandy-misra/main.cpp" "/home/jakub/dev/repos/chandy-misra/cmake-build-debug/CMakeFiles/chandy_misra.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/chandy_misra.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chandy_misra.dir/main.cpp.o"
+  "CMakeFiles/chandy_misra.dir/Coordinator.cpp.o"
+  "CMakeFiles/chandy_misra.dir/Fork.cpp.o"
+  "CMakeFiles/chandy_misra.dir/Philosopher.cpp.o"
+  "CMakeFiles/chandy_misra.dir/Table.cpp.o"
+  "CMakeFiles/chandy_misra.dir/ThreadSafePrinter.cpp.o"
+  "CMakeFiles/chandy_misra.dir/Visualisation.cpp.o"
+  "chandy_misra.pdb"
+  "chandy_misra"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chandy_misra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
